4d4373bff0625304b4b1af2dd59a04c407799053
Linette Listmann <llistman@tktk.ee>
2021-12-12 20:18:04 +0200

    Control flow

---
+++ Frozen/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Frozen
+{
+    class Program
+    {
+        public static object Stringsplitoptions { get; private set; }
+
+        class SecretSanta
+        {
+            string name;
+            string gift;
+
+            public SecretSanta(string _name, string _gift)
+            {
+                name = _name;
+                gift = _gift;
+
+            }
+
+            public string Name
+            {
+                get { return name; }
+
+            }
+
+            public string Gift
+            {
+                get { return gift; }
+            }
+
+        }
+
+        static void Main(string[] args)
+
+        {
+            List<SecretSanta> mySecretSanta = new List<SecretSanta>();
+            string[] secretsantaFromFile = GetDataFromFile();
+
+       
+            foreach (string line in secretsantaFromFile)
+            {
+
+                string[] temPArray = line.Split(new char[] { '/' });
+                SecretSanta newSecretSanta = new SecretSanta(temPArray[0], temPArray[1]);
+                mySecretSanta.Add(newSecretSanta);
+
+            }
+            foreach (SecretSanta secretsantafromList in mySecretSanta)
+
+            {
+
+                Console.WriteLine($" {secretsantafromList.Name} wants { secretsantafromList.Gift} for Christmas!");
+             
+
+            }
+        }
+
+
+        public static void DisplayElementsFromArray(string[] someArray)
+
+        {
+
+            foreach (string element in someArray)
+
+            {
+
+                Console.WriteLine($" String from array: {element}");
+
+            }
+
+        }
+
+        public static string[] GetDataFromFile()
+
+        {
+            string filePath = @"/Volumes/KÕVAKETAS/frozen.txt";
+
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OOPandFiles
+{
+    class Program
+    {
+        class Movie
+        {
+            string title;
+            string rating;
+            string year;
+
+            public Movie(string _title, string _rating, string _year)
+            {
+                title = _title;
+                rating = _rating;
+                year = _year;
+            }
+
+            //getters for Movie
+
+            public string Title
+            {
+                get { return title; } 
+
+            }
+
+            public string Rating
+            {
+                get { return rating;  }
+            }
+
+            public string Year
+            {
+                get { return year;  }
+            }
+
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            List<Movie> myMovies = new List<Movie>();
+            string[]moviesFromFile = GetDataFromFile();
+
+        }
+
+        public static void DisplayElementsFromArray (string[]someArray)
+
+        {
+            foreach (string element in someArray)
+            {
+
+                Console.WriteLine($"String from array: {element}");
+
+            }
+    
+        }
+
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"/Volumes/KÕVAKETAS/movies-2.txt";
+
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+
+        }
+
+}
+
+}
